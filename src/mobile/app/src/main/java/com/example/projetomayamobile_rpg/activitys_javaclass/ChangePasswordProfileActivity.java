dd5e3446f7feba0af21c378c3b1313f21f0c4376
Said Sales <cmcsaas@gmail.com>
2026-04-11 17:02:54 -0300

    iniciando tela de histórico e tela de alteração de senha feita

---
+++ src/mobile/app/src/main/java/com/example/projetomayamobile_rpg/activitys_javaclass/ChangePasswordProfileActivity.java
@@ -0,0 +1,15 @@
+package com.example.projetomayamobile_rpg.activitys_javaclass;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.projetomayamobile_rpg.R;
+
+public class ChangePasswordProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.change_password_profile_activity);
+    }
+}
